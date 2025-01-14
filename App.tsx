02de30c3aecbe9a05c19b64daada3e379ecd0cbd
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-14 15:03:30 -0300

    feat: configure StatusBar and add login screen image

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Login } from "./src/screens/Login";
+import { StatusBar, View } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="Login"
-      >
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <View style={{
+        paddingTop: getStatusBarHeight(),
+      }} >
+        <StatusBar
+          barStyle={"light-content"}
+          translucent
+          backgroundColor="#043484"
+        />
+      </View>
+
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Login"
+        >
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import { styles } from './styles';
 
 
 export function Login() {
   return (
     <View style={styles.container}>
+      <Image source={require("../../Assets/Images/login.png")} />
       <Text>ReactNative</Text>
     </View>
   );
