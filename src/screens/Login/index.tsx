dc174287cf333408a87f870195504a36f5085e02
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-14 17:42:32 -0300

    chore: install @expo/vector-icons

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { Image, StatusBar, Text, View } from "react-native";
+import { Image, StatusBar, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export function Login() {
   return (
-    <View style={styles.container}>
+    <View>
       <StatusBar
         barStyle={"light-content"}
         translucent
         backgroundColor="#043484"
       />
       <Image source={require("../../Assets/Images/login.png")} />
-      <Text>ReactNative</Text>
+
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>Bem-vindo ao CSC Eduacation</Text>
+        <Text style={styles.signText}>Login/ Signup</Text>
+
+        <TouchableOpacity>Sign with Google</TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -4,5 +4,21 @@ import { getStatusBarHeight } from "react-native-iphone-x-helper";
 export const styles = StyleSheet.create({
   container: {
     paddingTop: getStatusBarHeight(),
+    marginTop: -25,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+
+  welcomeText: {
+    fontSize: 35,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+
+  signText: {
+    textAlign: "center",
+    marginTop: 40,
+    fontSize: 20,
   },
 });
